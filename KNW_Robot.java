29c8f81376cfa9bfac2b74efd6e4a5e8e90cf177
Dyeity <sjdye@smu.edu>
2017-09-24 19:08:29 -0500

    *Very* basic pseudo code for functions we had in the UML
    *shrugs*

--- KNW_Robot.java
+++ KNW_Robot.java
@@ -1,7 +1,42 @@
+import RTXTRobot.*
+
 public class KNW_Robot {
+
+    //variables for probe data
 
     public static void main()
     {
-        //booom
+        //sequential function calls for whatever course
     }
+
+    //void move --move until obstacle is hit
+    public static void move()
+    {
+
+    }
+
+    //void move(float)
+    public static void move(float distance)
+    {
+
+    }
+
+    //void turn(int)    --Left:  1, Right: 2
+    public static void turn(int direction)
+    {
+
+    }
+
+    //void senseBarrier(float)
+    
+    //void senseGap
+    //float senseTemp
+    //float senseWind
+    //void raiseLift
+    //void lowerLift
+    //void dropArm
+    //float senseWater
+    //void releaseBeacon
+    //void outputData   --read out to terminal, I guess)
+    //void storeData    --would store to external file)
 }
