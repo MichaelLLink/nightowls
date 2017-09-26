a82914c2552cbfa80e8012cc02f8ba71627f37d7
Dyeity <sjdye@smu.edu>
2017-09-25 21:18:31 -0500

    Rudimentary Move and RunTillBump programs

---
+++ Sprint 2/RunTillBump/src/RunTillBump.java
@@ -0,0 +1,37 @@
+import RXTXRobot.*;
+
+public class RunTillBump {
+
+    public static int main()
+    {
+        boolean bumpTriggered = false;
+
+
+        //set up arduino
+        RXTXRobot robot = new ArduinoUno();
+        robot.setPort(/dev/tty.usbmodem411); //mac port???
+        robot.connect();
+
+        while (!bumpTriggered)
+        {
+            //check bump sensor, which would be in a DIGITAL port
+            robot.refreshDigitalPins();
+            int reading = robot.getDigitalPin(11).getValue(); //IDK what value signifies not pushed
+
+            //add if statement to set bumpTriggered based on reading
+
+            if(!bumpTriggered)
+            {
+                //run motor
+                //runEncodedMotor(robot.MOTOR1, 500, 0, robot.MOTOR2, 500, 0);  //if you can't call an encoded motor directly, we gotta figure out how to use this
+                runMotor(robot.MOTOR1, 500, 0, robot.MOTOR2, 500, 0);
+            }
+
+        }
+        //close robot
+        robot.close();
+
+
+    }
+
+}
