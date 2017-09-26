1a596747f8e18587cc42bc6404313cc9dedee415
Dyeity <sjdye@smu.edu>
2017-09-25 20:30:13 -0500

    Basic Move function

--- Sprint 2/Move/src/Move.java
+++ Sprint 2/Move/src/Move.java
@@ -1,7 +1,30 @@
-include RXTXRobot.*;
+include rxtxrobot.*;
 
-public class Move {
+public class Move
+{
+
+    public static int main()
+    {
+            //set up arduino
+        RXTXRobot robot = new ArduinoUno();
+        robot.setPort(/dev/tty.usbmodem411); //mac port???
+        robot.connect();
+
+            //motors 1 and 2 are already set up so we don't need to do that
+
+        //distance in inches
+        int distance = 3;
+
+            //set ticks (11 being our ticks-per-inches ratio)
+        int ticks = 11 * distance;
+
+            //move 3 feet
+        runEncodedMotor(robot.MOTOR1, 500, ticks, robot.MOTOR2, 500, ticks);
+
+            //close robot
+        robot.close();
 
 
+    }
 
 }
