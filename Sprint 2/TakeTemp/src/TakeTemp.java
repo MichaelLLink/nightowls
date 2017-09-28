37b23d390cc857035d0e5b6161b329b6e3f3dc99
Dyeity <sjdye@smu.edu>
2017-09-27 20:23:55 -0500

    In case my computer dies

--- Sprint 2/TakeTemp/src/TakeTemp.java
+++ Sprint 2/TakeTemp/src/TakeTemp.java
@@ -8,7 +8,7 @@ public class TakeTemp
     public static void main(String[] args)
     {
         robot = new ArduinoUno();
-        robot.setPort("COM3"); //unsure about port
+        robot.setPort("/dev/tty.usbmodem411"); //unsure about port
         robot.connect();
 
         int thermistorReading = getThermistorReading();
