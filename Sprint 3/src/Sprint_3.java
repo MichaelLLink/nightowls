2c816ea29d15133fffdee6b50b39754f9cd7b5d9
Dyeity <sjdye@smu.edu>
2017-10-12 20:45:19 -0500

    Sprint 3 program messed with

--- Sprint 3/src/Sprint_3.java
+++ Sprint 3/src/Sprint_3.java
@@ -4,17 +4,17 @@ public class Sprint_3 {
 
     public static RXTXRobot robot;
     public static int speed;
-    public static int pingFrontPin;
+    //public static int pingFrontPin;
     //public static int pingLeftPin;
-    //public static int pingRightPin;
+    public static int pingRightPin;
     public static int bumpPin;
     public static int tempPin;
     public static int windPin;
     //public static int conductivityPin;
-    public static int armPin;
+    //public static int armPin;
     //public static int gyroscopePin
     //public static int probePin;
-    //public static int dumpPin;
+    public static int dumpPin;
 
     //calibrations
     public static double exposedSlope;
@@ -23,24 +23,24 @@ public class Sprint_3 {
     public static double windIntercept;
 
     public static void main(String[] args) {
-        //set up robot
+            //set up robot
         robot = new ArduinoUno();
         robot.setPort("/dev/tty.usbmodem1411"); //make sure to update port before running
         robot.connect();
 
-        //set pin and other static variables
-        pingPin = 7;    //digital pin
+            //set pin and other static variables
+        pingRightPin = 7;    //digital pin
         bumpPin = 3;   //analog pin
         tempPin = 0;    //analog
         windPin = 1;  //analog
         //conductivityPin = ;   //Digital: D12, D13     Analog: A4, A5
-        armPin = 9;     //digital
+        dumpPin = 9;     //digital
         //gyroscopePin
         //probePin
         //dumpPin
 
 
-        //calibrations
+            //calibrations
         exposedSlope = -6.58594486;
         exposedIntercept = 687.9718646;
         windSlope = -15.595;
@@ -48,15 +48,15 @@ public class Sprint_3 {
 
         speed = 200;
 
-        //set up motors and sensors
+            //set up motors and sensors
         robot.attachMotor(RXTXRobot.MOTOR1, 5);
         robot.attachMotor(RXTXRobot.MOTOR2, 6);
-        robot.attachServo(RXTXRobot.SERVO1, armPin);
+        robot.attachServo(RXTXRobot.SERVO1, dumpPin);
         //robot.attachServo(RXTXRobot.SERVO2,probePin);
         //robot.attachServo(RXTXRobot.SERVO3,dumpPin);
 
 
-        //run through the course
+            //run through the course
 
 
         robot.close();
@@ -86,7 +86,7 @@ public class Sprint_3 {
 
     //public static [return] takeConductivity(){}
 
-    /*
+
     public static int getThermistorReading()
     {
         int sum = 0;
@@ -116,5 +116,5 @@ public class Sprint_3 {
 
         return sum / readingCount;
     }
-    */
+
 }
