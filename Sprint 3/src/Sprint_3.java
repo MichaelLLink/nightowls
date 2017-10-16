87fe9a71df38c0c1ac9ca237b3f60007d6ce543c
Dyeity <sjdye@smu.edu>
2017-10-15 21:02:12 -0500

    Several functions moved in and done

--- Sprint 3/src/Sprint_3.java
+++ Sprint 3/src/Sprint_3.java
@@ -1,5 +1,7 @@
 import rxtxrobot.*;
 
+import java.util.Scanner;
+
 public class Sprint_3 {
 
     public static RXTXRobot robot;
@@ -13,8 +15,13 @@ public class Sprint_3 {
     //public static int conductivityPin;
     //public static int armPin;
     //public static int gyroscopePin
-    //public static int probePin;
+    //public static int boomPin;
     public static int dumpPin;
+
+        //data
+    public static double windSpeed;
+    public static double temp;
+    public static float conductivity;
 
     //calibrations
     public static double exposedSlope;
@@ -52,8 +59,8 @@ public class Sprint_3 {
         robot.attachMotor(RXTXRobot.MOTOR1, 5);
         robot.attachMotor(RXTXRobot.MOTOR2, 6);
         robot.attachServo(RXTXRobot.SERVO1, dumpPin);
-        //robot.attachServo(RXTXRobot.SERVO2,probePin);
-        //robot.attachServo(RXTXRobot.SERVO3,dumpPin);
+        //robot.attachServo(RXTXRobot.SERVO2,boomPin);
+        //robot.attachServo(RXTXRobot.SERVO3,armPin);
 
 
             //run through the course
@@ -64,28 +71,109 @@ public class Sprint_3 {
 
     //public static void moveTillSense(){}
 
-    //public static void moveTillBump(){}
+    public static void move(int distance)
+    {
+        int ticks = distance*11;
 
-    //public static void move(float distance){}
+        robot.runEncodedMotor(RXTXRobot.MOTOR1, speed, ticks, RXTXRobot.MOTOR2, -speed, ticks);
 
-    //public static void turn(int direction){}
+    }
 
-    //public static void raiseSensors(){}
+    public static void turn(int direction)
+    {
+        if(direction == 1) //left
+        {
+            //robot.runEncodedMotor(RXTXRobot.MOTOR1, -speed, RXTXRobot.MOTOR2, -speed, [man idk]);
+            robot.runMotor(RXTXRobot.MOTOR1, -speed, RXTXRobot.MOTOR2, -speed, 2000);
+        }
+        else if(direction == 2) //right
+        {
+            //robot.runEncodedMotor(RXTXRobot.MOTOR1, speed, RXTXRobot.MOTOR2, speed, [man idk]);
+            robot.runMotor(RXTXRobot.MOTOR1, speed, RXTXRobot.MOTOR2, speed, 2000);
+        }
+    }
 
-    //public static void lowerSensors(){}
+    public static void raiseBoom()
+    {
+        robot.moveServo(RXTXRobot.SERVO2, 180);
+    }
 
-    //public static void lowerArm(){}
+    public static void lowerBoom()
+    {
+        robot.moveServo(RXTXRobot.SERVO2, 0);
+    }
 
-    //public static void raiseArm(){}
+    public static void lowerArm()
+    {
+        robot.moveServo(RXTXRobot.SERVO3, 0);
+    }
 
-    //public static void deployBeacon(){}
+    public static void raiseArm()
+    {
+        robot.moveServo(RXTXRobot.SERVO3, 180);
+    }
 
-    //public static [return] takeTemp(){}
+    public static void deployBeacon()
+    {
+        robot.moveServo(RXTXRobot.SERVO1, 145);
+    }
 
-    //public static [return] takeWindSpeed(){}
+    //public static void takeConductivity(){}
 
-    //public static [return] takeConductivity(){}
+    public static void changeAngle(int angle) {
+        //move servo
+        robot.moveServo(RXTXRobot.SERVO1, angle);
+    }
 
+    public static void runTillBump()
+    {
+        boolean bumpTriggered = false;
+
+        robot.runMotor(RXTXRobot.MOTOR1, speed, RXTXRobot.MOTOR2, -speed, 0);
+
+        while (!bumpTriggered)
+        {
+            robot.refreshAnalogPins();
+            int reading = robot.getAnalogPin(bumpPin).getValue(); //IDK what value signifies not pushed
+
+            //add if statement to set bumpTriggered based on reading
+            if(reading == 0) {
+                bumpTriggered = true;
+                System.out.println(reading);
+                System.out.println("bump triggered");
+            }
+
+            if(bumpTriggered)
+            {
+                //stop motor
+                robot.runMotor(RXTXRobot.MOTOR1, 0, RXTXRobot.MOTOR2, 0, 0);
+            }
+
+        }
+    }
+
+    public static void senseDistance(int pin)
+    {
+        robot.refreshDigitalPins();
+        int distance = robot.getPing(pin); //remember to check pin
+
+        //read out distance
+        System.out.println("Distance: " + distance + "cm");
+    }
+
+    public static void takeTemp()
+    {
+        double thermistorReading = getThermistorReading();
+
+        //System.out.println("The probe read the value: " + thermistorReading);
+        //System.out.println("In volts: " + (thermistorReading * (5.0/1023.0)));
+
+        double temp = 0;
+
+        temp = (thermistorReading - exposedIntercept)/exposedSlope;
+
+        System.out.println("The temperature is: " + temp + " celsius");
+    }
 
     public static int getThermistorReading()
     {
@@ -100,6 +188,24 @@ public class Sprint_3 {
         }
 
         return sum / readingCount;
+    }
+
+    public static void getWindSpeed()
+    {
+        double anemometerReading = getAnemometerReading();
+        double thermistorReading = getThermistorReading();
+
+        double windSpeed = ((thermistorReading-anemometerReading)-windIntercept)/windSlope;
+
+        //not entirely sure what we need here
+        System.out.println("The shielded thermistor read the value: " + anemometerReading);
+        //System.out.println("In volts: " + (anemometerReading * (5.0/1023.0)));
+
+        System.out.println("The exposed thermistor read the value: " + thermistorReading);
+        //System.out.println("In volts: " + (thermistorReading * (5.0/1023.0)));
+
+        System.out.println("The wind speed is: " + windSpeed + "m/s");
+
     }
 
     public static int getAnemometerReading()
